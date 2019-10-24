16b1cbefe7dfc3fdcde480c7dac4091e26f734c3
xwOvO <495974017@qq.com>
2019-10-24 16:46:54 +0800

    Update float-equal.c

--- float-equal.c
+++ float-equal.c
@@ -1,12 +1,13 @@
 //实现代码判断2个float变量相等
 //误差精确到时小数点后5位
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z == z1) {
+    if (abs(m-x) <= 0.000001) {
         z = (m + x) / y;
     }
     printf("Z is %f\n", z);
