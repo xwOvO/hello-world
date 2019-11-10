0156bad51b7c0cafe65133569f3a6c8946c72e11
xwOvO <495974017@qq.com>
2019-11-10 12:46:57 +0800

    第十周作业
    第十周作业

--- max_5_prime.c
+++ max_5_prime.c
@@ -1,3 +1,30 @@
-//小明有 5 张数字卡片，上面分别为数字 1， 2， 3， 6， 9。小明想用这 5 张数字卡片组成一个最大的 5 位
-//素数，这个五位数是_______。
-//编程：任意输入5个数字，将它们组合为最大的5位素数输出。如果不存在5位素数，则输出ERROR。
+#include <stdio.h>
+int main(){
+	int i,j;//ѭ����
+	int num[4];
+	while(i<5){
+		scanf("%d",&num[i]);
+		i++;
+	} 
+	for(i=0;i<5;i++){
+		for(j=i;j<5;j++){
+			if(num[i]<num[j]){
+				int temp=num[i];
+				num[i]=num[j];
+				num[j]=temp;
+			}
+		}
+	}
+	int value=10000*num[0]+1000*num[1]+100*num[2]+10*num[3]+1*num[4];
+	for(j=2;j<value;j++){
+		i=1;
+		if(value%j==0){
+			value=value-i;
+		}else
+		{
+			printf("%d",value);
+			break;
+		}
+	}
+	return 0;
+}
