3a69910914d9daa8fa9f947dd81d61bb56cb364a
xwOvO <495974017@qq.com>
2019-11-12 11:52:21 +0800

    Update google.c

--- google.c
+++ google.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-
 int main(){
-	int i;//Ñ­»·Êı 
-	int n;
-	int num=1;
-	int m;//´æ´¢n/10
-	int k;//´æ´¢n%10 
-	for(n=10;n>0;n++){
-		m=n;k=n;
-		for(i=0;i>-1;i++){
-			k=k%10;
-			m=m/10;
-			if(m==0){
-				break;
-			}
-			if(m==1){
-				num++;
-			}
-			if(k==1){
-				num++;
-			}
-		}
-		if(n==num)
-			break;
-	}
-	printf("n=%d\n",n);
-	return;
+ int i;//Ñ­»·Êı
+ int n;
+ int num=1;
+ int m;//´æ´¢n/10
+ int k;//´æ´¢n%10
+ for(n=2;n>0;n++){
+  m=n;
+  for(i=0;i>-1;i++){
+   k=m%10;
+   m=m/10;
+   if(k==1){
+    num++;
+   }
+   if(m==0){
+    break;
+   }
+   if(m==1){
+    num++;break;
+   }
+  }
+  if(n==num)
+   break;
+ }
+ printf("n=%d\n",n);
+ return 0;
 }
