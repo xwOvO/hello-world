b8586ed5ca00cb7bc7b3dfcddb26a7edbd02cecc
xwOvO <55223847+xwOvO@users.noreply.github.com>
2020-01-09 01:04:08 +0800

    第十三周作业
    第十三周作业

--- SuperPrime.cpp
+++ SuperPrime.cpp
@@ -1,81 +1,50 @@
-class Number {
-public:
-  Number(int num):mNum(num) {
-  }
-  Number(const Number &n):mNum(n.mNum) {
-  }
-  ~Number();
-  NumberSet split() {
-  	NumberSet ns;
-  	int n = mNum;
-  	while(n != 0) {
-  	  int d = n % 10;
-  	  Number dig(d);
-  	  ns.add(dig);
-  	  n = n / 10;
+//超级素数：它本身，各位数字的和，各位数字的平方和，都是素数。
+//求 100~10000 内的所有超级素数，及它们的平均数
+#include <stdio.h>
+#include <stdlib.h>
+int judgePrime(int );
+int main(){
+	int primeNum[10000];//存放所有素数的数组 
+	int superPrimeNum[10000];
+	int index_primeNum = 0;      //primeNum的索引 
+	int index_superPrimeNum = 0;
+	int num;            //num将从100 ~ 10000;
+	int i;
+	float averager = 0;
+	for(num = 100;num <= 10000;num++){
+		if(judgePrime(num) == num){
+			primeNum[index_primeNum] = num;
+			index_primeNum++;
+		}
 	}
-	return ns;
-  }
-  bool isPrime() {
-  	for(int i = 2; i < mNum; ++i) {
-  	  if(mNum % i == 0)
-  	    break;
+	for(i = 0;i < index_primeNum;i++){
+		int prime = primeNum[i];
+		int prime_digital_sum = (prime%10)+((prime/10)%10)+((prime/100)%10)+(prime/1000);
+		int prime_digital_squarsum = (prime%10)*(prime%10)+((prime/10)%10)*((prime/10)%10)+((prime/100)%10)*((prime/100)%10)+(prime/1000)*(prime/1000);
+		if(judgePrime(prime_digital_sum) == prime_digital_sum && judgePrime(prime_digital_squarsum) == prime_digital_squarsum){
+			superPrimeNum[index_superPrimeNum] = prime;
+			index_superPrimeNum++;
+		}
 	}
-	if(i != mNum)
-	  return false;
-	return true;
-  }
-private:
-  const int mNum;
-};
-class NumberSet {
-private:
-  Number *nums[20];
-  int size;
-public:
-  NumberSet() {
-  	size = 0;
-  	for(int i = 0; i < 20; ++i)
-  	  nums[i] = NULL;
-  }
-  ~NumberSet() {
-  	for(int i = 0; i < size; ++i)
-  	  delete nums[i];
-  }
-  bool add(const Number &n) {
-  	if(size != 20 && nums[size] == NULL) {
-	 nums[size] = new Number(n); 
-     size += 1;
-     return true;
-    }
-    return false;
-  }
-  Number sum() {
-  	Number sum(0);
-  	for(int i = 0; i < size; ++i) {
-  	  if(nums[i] != NULL)
-  	    sum.add(*(nums[i]));
+	for(i = 0;i < index_superPrimeNum;i++){
+		printf("%d\n",superPrimeNum[i]);
+		averager += superPrimeNum[i];
 	}
-	return sum;
-  }
-};
-class SuperPrime : public Number {
-public:
-  SuperPrime(int num);
-  ~SuperPrime();
-
-  bool isPrime();
-
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
-private:
-  const int num;
+	printf("%f",averager/index_superPrimeNum);
+	system("pause");
+	return 0;
 }
-int main()
-{
-    SuperPrime sp(113);
-    if(sp.isPrime()) {
-        ; // do something
-    }
+int judgePrime(int num){
+	int i;
+	int times = 0;
+	for(i = 2;i < num;i++){
+		if(num % i == 0){
+			times++;
+		}
+	}
+	if(times != 0){
+		return 0;
+	}else{
+		return num;
+	}
 }
